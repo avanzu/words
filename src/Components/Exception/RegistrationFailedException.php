dfe534bda2886d6fb2b8a279516bea47cf8bbb67
Marc Bach <mail@avanzu.de>
2017-04-29 14:15:41 +0200

    Adds exception classes

---
+++ src/Components/Exception/RegistrationFailedException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * RegistrationFailedException.php
+ * restfully
+ * Date: 29.04.17
+ */
+
+namespace Components\Exception;
+
+
+class RegistrationFailedException extends \Exception
+{
+
+}
