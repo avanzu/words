9cb5b3974211c3b5f967251081b5f950511a538c
Marc Bach <mail@avanzu.de>
2018-01-11 22:28:14 +0100

    adds trans unit state

--- src/Components/Model/TransValue.php
+++ src/Components/Model/TransValue.php
@@ -11,11 +11,40 @@ namespace Components\Model;
 class TransValue
 {
 
+    const STATE_NONE                     = null;
+    const STATE_FINAL                    = 'final';
+    const STATE_NEEDS_ADAPTATION         = 'needs-adaptation';
+    const STATE_NEEDS_L10N               = 'needs-l10n';
+    const STATE_NEEDS_REVIEW_ADAPTATION  = 'needs-review-adaptation';
+    const STATE_NEEDS_REVIEW_L10N        = 'needs-review-l10n';
+    const STATE_NEEDS_REVIEW_TRANSLATION = 'needs-review-translation';
+    const STATE_NEEDS_TRANSLATION        = 'needs-translation';
+    const STATE_NEW                      = 'new';
+    const STATE_SIGNED_OFF               = 'signed-off';
+    const STATE_TRANSLATED               = 'translated';
+
     protected $unit;
 
     protected $locale;
 
     protected $content;
+
+    protected $state;
+
+    /**
+     * TransValue constructor.
+     *
+     * @param $locale
+     * @param $content
+     * @param $state
+     */
+    public function __construct($locale = null, $content = '', $state = TransValue::STATE_NONE)
+    {
+        $this->locale  = $locale;
+        $this->content = $content;
+        $this->state   = $state;
+    }
+
 
     /**
      * @return mixed
@@ -60,6 +89,31 @@ class TransValue
     /**
      * @return mixed
      */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param mixed $state
+     *
+     * @return $this
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return (string)$this->getContent();
+    }
+
+    /**
+     * @return mixed
+     */
     public function getContent()
     {
         return $this->content;
@@ -75,11 +129,6 @@ class TransValue
         $this->content = $content;
 
         return $this;
-    }
-
-    public function __toString()
-    {
-        return (string)$this->getContent();
     }
 
 
