cc23d547493a9251427b207853d4743d9a39538f
Marc Bach <mail@avanzu.de>
2018-01-11 22:26:24 +0100

    Adds install migration

--- app/Migrations/Version20180109182221.php
+++ app/Migrations/Version20180109182221.php
@@ -10,9 +10,42 @@ use Doctrine\DBAL\Schema\Schema;
  */
 class Version20180109182221 extends AbstractMigration
 {
+
     public function up(Schema $schema)
     {
 
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE access_token (id INT AUTO_INCREMENT NOT NULL, client_id INT DEFAULT NULL, user_id INT DEFAULT NULL, token VARCHAR(255) NOT NULL, expires_at INT DEFAULT NULL, scope VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_B6A2DD685F37A13B (token), INDEX IDX_B6A2DD6819EB6921 (client_id), INDEX IDX_B6A2DD68A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE auth_code (id INT AUTO_INCREMENT NOT NULL, client_id INT DEFAULT NULL, user_id INT DEFAULT NULL, token VARCHAR(255) NOT NULL, redirect_uri LONGTEXT NOT NULL, expires_at INT DEFAULT NULL, scope VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_5933D02C5F37A13B (token), INDEX IDX_5933D02C19EB6921 (client_id), INDEX IDX_5933D02CA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, random_id VARCHAR(255) NOT NULL, redirect_uris LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', secret VARCHAR(255) NOT NULL, allowed_grant_types LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE refresh_token (id INT AUTO_INCREMENT NOT NULL, client_id INT DEFAULT NULL, user_id INT DEFAULT NULL, token VARCHAR(255) NOT NULL, expires_at INT DEFAULT NULL, scope VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_C74F21955F37A13B (token), INDEX IDX_C74F219519EB6921 (client_id), INDEX IDX_C74F2195A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, canonical VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, UNIQUE INDEX uidx_canonical (canonical), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE trans_unit (id INT AUTO_INCREMENT NOT NULL, project_id INT DEFAULT NULL, token VARCHAR(255) NOT NULL, catalogue VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, source_string LONGTEXT DEFAULT NULL, INDEX IDX_CFBFC560166D1F9C (project_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE trans_value (id INT AUTO_INCREMENT NOT NULL, unit_id INT DEFAULT NULL, locale VARCHAR(10) NOT NULL, content LONGTEXT DEFAULT NULL, state VARCHAR(50) DEFAULT NULL, INDEX IDX_A2B43DEBF8BD700D (unit_id), INDEX idx_locale (locale), INDEX idx_state (state), INDEX ics_locale_state (locale, state), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, is_active TINYINT(1) NOT NULL, roles JSON DEFAULT NULL COMMENT \'(DC2Type:json_array)\', token VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE access_token ADD CONSTRAINT FK_B6A2DD6819EB6921 FOREIGN KEY (client_id) REFERENCES client (id)');
+        $this->addSql('ALTER TABLE access_token ADD CONSTRAINT FK_B6A2DD68A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE auth_code ADD CONSTRAINT FK_5933D02C19EB6921 FOREIGN KEY (client_id) REFERENCES client (id)');
+        $this->addSql('ALTER TABLE auth_code ADD CONSTRAINT FK_5933D02CA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE refresh_token ADD CONSTRAINT FK_C74F219519EB6921 FOREIGN KEY (client_id) REFERENCES client (id)');
+        $this->addSql('ALTER TABLE refresh_token ADD CONSTRAINT FK_C74F2195A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE trans_unit ADD CONSTRAINT FK_CFBFC560166D1F9C FOREIGN KEY (project_id) REFERENCES project (id)');
+        $this->addSql('ALTER TABLE trans_value ADD CONSTRAINT FK_A2B43DEBF8BD700D FOREIGN KEY (unit_id) REFERENCES trans_unit (id)');
+
+        $this->addSql(
+            'insert ignore into project (canonical, name, description) values (:canonical, :name, :description)',
+            [
+                'canonical'   => 'common',
+                'name'        => 'Common translations',
+                'description' => 'Common translations are shared between all other projects and '
+                                 .'will act as the foundation of those translation files. '
+                                 .'Project specific translations will always take precedence'
+                                 .' (e.g. override the common translation).',
+            ]
+        );
+
+        /*
         $idQuery = $this
             ->connection
             ->createQueryBuilder()
@@ -29,6 +62,7 @@ class Version20180109182221 extends AbstractMigration
                 ['canonical' => 'common', 'name' => 'Common translations', 'description' => 'Common translations']
             );
         }
+        */
 
         $this->addSql('update trans_unit set project_id = (select id from project where canonical = :canonical) where project_id is null',
             ['canonical' => 'common'])
@@ -38,7 +72,24 @@ class Version20180109182221 extends AbstractMigration
 
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE access_token DROP FOREIGN KEY FK_B6A2DD6819EB6921');
+        $this->addSql('ALTER TABLE auth_code DROP FOREIGN KEY FK_5933D02C19EB6921');
+        $this->addSql('ALTER TABLE refresh_token DROP FOREIGN KEY FK_C74F219519EB6921');
+        $this->addSql('ALTER TABLE trans_unit DROP FOREIGN KEY FK_CFBFC560166D1F9C');
+        $this->addSql('ALTER TABLE trans_value DROP FOREIGN KEY FK_A2B43DEBF8BD700D');
+        $this->addSql('ALTER TABLE access_token DROP FOREIGN KEY FK_B6A2DD68A76ED395');
+        $this->addSql('ALTER TABLE auth_code DROP FOREIGN KEY FK_5933D02CA76ED395');
+        $this->addSql('ALTER TABLE refresh_token DROP FOREIGN KEY FK_C74F2195A76ED395');
+        $this->addSql('DROP TABLE access_token');
+        $this->addSql('DROP TABLE auth_code');
+        $this->addSql('DROP TABLE client');
+        $this->addSql('DROP TABLE refresh_token');
+        $this->addSql('DROP TABLE project');
+        $this->addSql('DROP TABLE trans_unit');
+        $this->addSql('DROP TABLE trans_value');
+        $this->addSql('DROP TABLE user');
 
     }
 }
