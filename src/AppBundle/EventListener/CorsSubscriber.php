18d2f9f898c07c3d47f3cdfe6d79ea3977cbf4a7
Marc Bach <mail@avanzu.de>
2018-01-14 16:21:21 +0100

    Adss handler for CORS header

---
+++ src/AppBundle/EventListener/CorsSubscriber.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * CorsSubscriber.php
+ * words
+ * Date: 14.01.18
+ */
+
+namespace AppBundle\EventListener;
+
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Class CorsSubscriber
+ */
+class CorsSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::REQUEST  => array('onKernelRequest', 9999),
+            KernelEvents::RESPONSE => array('onKernelResponse', 9999),
+        );
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $method = $event->getRequest()->getRealMethod();
+        if (Request::METHOD_OPTIONS !== $method) {
+            return;
+        }
+
+        $response = new Response();
+        $event->setResponse($response);
+
+    }
+
+    /**
+     * @param FilterResponseEvent $event
+     */
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $response = $event->getResponse();
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+
+
+}
